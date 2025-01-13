159bdaabbb85ade6720f01c0846cf3fce26b2619
imanukyan <idamyan01@gmail.com>
2025-01-13 22:39:31 +0100

    updated the logic

--- src/main/java/eu/civitalis/TemplateGenerator.java
+++ src/main/java/eu/civitalis/TemplateGenerator.java
@@ -10,30 +10,50 @@ import java.io.IOException;
 public class TemplateGenerator {
 
     public static void main(String[] args) {
+        // The directory to check for .doc files
+        File inputDir = new File("/Users/civitalis/Downloads/Datenverarbeitung/01_Bemessungsgrundlage/");
 
-        File inputDir = new File("path/to/your/doc/files");
-        File outputDir = new File("converted");
-        if (!outputDir.exists()) {
-            outputDir.mkdirs();
-        }
+        // Check if the directory exists and is a directory
+        if (inputDir.exists() && inputDir.isDirectory()) {
+            System.out.println("Directory found: " + inputDir.getAbsolutePath());
 
-        File[] docFiles = inputDir.listFiles((dir, name) -> name.endsWith(".doc"));
+            findDocFiles(inputDir);
 
-        if (docFiles != null) {
-            for (File docFile : docFiles) {
-                try {
-                    System.out.println("Converting: " + docFile.getName());
-                    convertor(docFile, outputDir);
-                } catch (IOException e) {
-                    e.printStackTrace();
+            // List all files in the directory with a .doc extension
+            File[] docFiles = inputDir.listFiles((dir, name) -> name.endsWith(".doc"));
+
+            // Check if there are any .doc files
+            if (docFiles != null && docFiles.length > 0) {
+                System.out.println("Found the following .doc files:");
+                for (File docFile : docFiles) {
+                    System.out.println(docFile.getName());
                 }
+            } else {
+                System.out.println("No .doc files found in the directory.");
             }
         } else {
-            System.out.println("No .doc files found in the specified directory.");
-
+            System.out.println("The specified path is not a valid directory.");
         }
     }
 
+    private static void findDocFiles(File dir) {
+        // Get all files and folders in the current directory
+        File[] files = dir.listFiles();
+
+        // Check if the list is not null and there are items in the directory
+        if (files != null) {
+            for (File file : files) {
+                // If it's a directory, recursively call the method
+                if (file.isDirectory()) {
+                    findDocFiles(file); // Recursive call
+                } else if (file.isFile() && file.getName().endsWith(".doc")) {
+                    // If it's a .doc file, print the file name
+                    System.out.println("Found .doc file: " + file.getAbsolutePath());
+                }
+            }
+
+        }
+    }
     private static void convertor(File docFile, File outputDir) throws IOException {
 
         try (FileInputStream fis = new FileInputStream(docFile)) {
@@ -41,6 +61,8 @@ public class TemplateGenerator {
 
             TemplateHelper.addTitle(document);
             TemplateHelper.addTableContent(document);
+            TemplateHelper.createSecondTable(document);
+            TemplateHelper.createThirdTable(document);
 
             File outputFile = new File(outputDir, docFile.getName().replace(".doc", ".docx"));
 
--- src/main/java/eu/civitalis/TemplateHelper.java
+++ src/main/java/eu/civitalis/TemplateHelper.java
@@ -16,11 +16,11 @@ public class TemplateHelper {
     public static void addTableContent(XWPFDocument document) {
         XWPFTable table = document.createTable(5, 2);
 
-        fillRow(table.getRow(0), "", "Retrieved from the mapping for ...");
-        fillRow(table.getRow(1), "Prozess", "Text after mapping for ...");
-        fillRow(table.getRow(2), "Releasestand", "Stand: ... after");
-        fillRow(table.getRow(3), "Letzte Anpassung an", "Stand: xxx, ... after");
-        fillRow(table.getRow(4), "Letzte Anpassung wegen", "Letzte Anpassung: A. Petroll");
+        fillRow(table.getRow(0), "", retrieveMappingNameFromDoc(document));
+        fillRow(table.getRow(1), "Prozess", retrieveProzessNameFromDoc(document));
+        fillRow(table.getRow(2), "Releasestand", retrieveReleasestandFromDoc(document));
+        fillRow(table.getRow(3), "Letzte Anpassung an", retrieveAnpassungAnFromDoc(document));
+        fillRow(table.getRow(4), "Letzte Anpassung wegen", retrieveAnpassungWegenFromDoc(document));
     }
 
     private static void fillRow(XWPFTableRow row, String cell1Text, String cell2Text) {
@@ -28,7 +28,45 @@ public class TemplateHelper {
         row.getCell(1).setText(cell2Text);
     }
 
-    private static void retrieveFromDoc(){
+    public static void createSecondTable(XWPFDocument document){
+
+        XWPFTable table = document.createTable(4,2);
+
+        fillRow(table.getRow(0), "Filterbedingung", "");
+        fillRow(table.getRow(1), "Join-Bedingungen", "");
+        fillRow(table.getRow(2), "Hilfsvariablen", "");
+        fillRow(table.getRow(3), "Tabellenname/View", "");
+    }
+
+    public static void createThirdTable(XWPFDocument document){
+
+        XWPFTable table = document.createTable(4,2);
+
+        fillRow(table.getRow(0), "Attributname", "Logik");
+        retrieveFromDocMainTable(document);
+    }
+
+    private static String retrieveMappingNameFromDoc(XWPFDocument document){
+        return "";
+    }
+
+    private static String retrieveProzessNameFromDoc(XWPFDocument document){
+        return "";
+    }
+
+    private static String retrieveReleasestandFromDoc(XWPFDocument document){
+        return "";
+    }
+
+    private static String retrieveAnpassungAnFromDoc(XWPFDocument document){
+        return "";
+    }
+
+    private static String retrieveAnpassungWegenFromDoc(XWPFDocument document){
+        return "";
+    }
+
+    private static void retrieveFromDocMainTable(XWPFDocument document){
 
     }
 }
